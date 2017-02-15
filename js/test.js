78dd1d22352cd7d066a26121b0b2d46034ba468c
Oleg Torokhov <torokhov093@gmail.com>
2017-02-15 11:31:34 +0700

    correct test for func readFromFile

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 describe("readData", function() {
   describe("readFromFile", function() {
-    it("Чтение данных", function() {
+    it("Получение строки с данными из файла", function() {
       assert.equal(readFromFile("js/data/dataExample.json"), '[{"Id": 0, "City": "г. Белинский"}, {"Id": 1, "City": "г. Каменка"}, {"Id": 2, "City": "ЗАТО п. Солнечный"}, {"Id": 3, "City": "р.п. Тамала"}, {"Id": 4, "City": "Антропшино (Сусанинское СП)"}, {"Id": 5, "City": "Адыгейск"}, {"Id": 6, "City": "Майкоп"}, {"Id": 7, "City": "Иткуловский 1-й"}]');
     });
   });
