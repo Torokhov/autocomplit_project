a390f7d6daa3a3393d185166d8242af58533b72e
Oleg Torokhov <torokhov093@gmail.com>
2017-03-08 13:14:08 +0700

    add asyns req

--- js/autocomplit.js
+++ js/autocomplit.js
@@ -1,17 +1,27 @@
-function readFromFile(path) {
-  var xhr = new XMLHttpRequest();
+function get(path) {
+  return new Promise(function(succeed, fail) {
+    var xhr = new XMLHttpRequest();
   
-  xhr.open('GET', path, false);
-  xhr.send();
+    xhr.open('GET', path, true);
+    xhr.addEventListener("load", function() {
+      if (xhr.status < 400) {
+        succeed(xhr.responseText);
+      } else {
+        fail(new Error("Request failed: " + xhr.statusText));
+      }
+    });
   
-  if (xhr.status != 200) {
-      console.log('ошибка: ' + (this.status ? this.statusText : 'запрос не удался'));
-      return;
-  } else {
-    return xhr.responseText;
-  }
+  xhr.addEventListener("error", function() {
+    fail(new Error("Network error"));
+  });
+  xhr.send(null);
+  });
 }
 
-function parse(str) {
-  return JSON.parse(str);
+function parse(path) {
+  get(path).then(function(text) {
+    return JSON.parse(text);
+  }, function(error) {
+    console.log("Failed to fetch: " + error);
+  });
 }
