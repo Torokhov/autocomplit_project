3679c00874b6023db432011325ab2c8d4111f88b
Oleg Torokhov <torokhov093@gmail.com>
2017-03-08 15:49:58 +0700

    add click event for variants and input event for autocomplite-field

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+var autocompliteField = document.getElementById("autocomplit-field");
+
+var variants = document.getElementById("variants");
+
+variants.onclick = function(event) {
+  var target = event.target;
+  
+  if (target.tagName === "LI") {
+    autocompliteField.value = target.textContent;
+    this.removeChild(this.firstChild);
+  }
+};
+
+autocompliteField.addEventListener("input", function(event) {
+  function f(text) {
+    var data = JSON.parse(text);
+    
+    var reg = new RegExp(this.value, "ig");
+    
+    data = data.filter(isRegularTrue);
+    
+    variants.textContent = "";
+    variants.appendChild(CreateList(data));
+    
+    function isRegularTrue(value) {
+      return value.City.search(reg) > 0;
+    };
+    
+    function CreateList(data) {
+      var fragment = document.createDocumentFragment();
+      var elem;
+      
+      data.forEach(function(value) {
+        elem = document.createElement("li");
+        elem.textContent = value.City;
+        
+        fragment.appendChild(elem);
+      });
+      
+      var list = document.createElement("ul");
+      list.appendChild(fragment);
+      
+      return list;
+    };
+  };
+  
+  var g = f.bind(this);
+  
+  get("data/kladr.json").then(g);
+}); 
