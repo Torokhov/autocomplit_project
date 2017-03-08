1865f536177784eca6406b71167898c82fe081e5
Oleg Torokhov <torokhov093@gmail.com>
2017-03-08 13:13:41 +0700

    test refactoring, add asyns

--- js/test.js
+++ js/test.js
@@ -1,46 +1,40 @@
 describe("Чтение данных", function() {
-  describe("readFromFile", function() {
-    it("Получение строки с данными из файла", function() {
-      assert.equal(readFromFile("http://127.0.0.1:65077/data/dataExample.json"), '[\r\n    {\r\n      "Id": 0,\r\n      "City": "г. Белинский"\r\n    },\r\n    {\r\n      "Id": 1,\r\n      "City": "г. Каменка"\r\n    },\r\n    {\r\n      "Id": 2,\r\n      "City": "ЗАТО п. Солнечный"\r\n    },\r\n    {\r\n      "Id": 3,\r\n      "City": "р.п. Тамала"\r\n    },\r\n    {\r\n      "Id": 4,\r\n      "City": "Антропшино (Сусанинское СП)"\r\n    },\r\n    {\r\n      "Id": 5,\r\n      "City": "Адыгейск"\r\n    },\r\n    {\r\n      "Id": 6,\r\n      "City": "Майкоп"\r\n    },\r\n    {\r\n      "Id": 7,\r\n      "City": "Иткуловский 1-й"\r\n    }\r\n]\r\n');
-    });
-  });
-  
-  describe("parse", function() {
-    it("Парсинг данных", function() {
-      data = readFromFile("http://127.0.0.1:65077/data/dataExample.json");
-      assert.deepEqual(parse(data), [
-          {
-            "Id": 0,
-            "City": "г. Белинский"
-          },
-          {
-            "Id": 1,
-            "City": "г. Каменка"
-          },
-          {
-            "Id": 2,
-            "City": "ЗАТО п. Солнечный"
-          },
-          {
-            "Id": 3,
-            "City": "р.п. Тамала"
-          },
-          {
-            "Id": 4,
-            "City": "Антропшино (Сусанинское СП)"
-          },
-          {
-            "Id": 5,
-            "City": "Адыгейск"
-          },
-          {
-            "Id": 6,
-            "City": "Майкоп"
-          },
-          {
-            "Id": 7,
-            "City": "Иткуловский 1-й"
-          }
+  it("get", function() {
+    get("data/dataExample.json").then(function(text) {
+      var data = JSON.parse(text);
+      assert.deepEqual(data, [
+        {
+          "Id": 0,
+          "City": "г. Белинский"
+        },
+        {
+          "Id": 1,
+          "City": "г. Каменка"
+        },
+        {
+          "Id": 2,
+          "City": "ЗАТО п. Солнечный"
+        },
+        {
+          "Id": 3,
+          "City": "р.п. Тамала"
+        },
+        {
+          "Id": 4,
+          "City": "Антропшино (Сусанинское СП)"
+        },
+        {
+          "Id": 5,
+          "City": "Адыгейск"
+        },
+        {
+          "Id": 6,
+          "City": "Майкоп"
+        },
+        {
+          "Id": 7,
+          "City": "Иткуловский 1-й"
+        }
       ]);
     });
   });
