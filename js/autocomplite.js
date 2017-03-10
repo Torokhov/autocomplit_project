76eaf95b938eff5c50af4bee4f5d0b7c6028b8cf
Oleg Torokhov <torokhov093@gmail.com>
2017-03-10 18:55:19 +0700

    rename autocomplit.js

--- js/autocomplite.js
+++ js/autocomplite.js
@@ -1,16 +1,33 @@
-function Autocomplite() {
+function Autocomplite(path) {
+  this.path = path;
   var data;
   
-  this.getData = function(path) {
+  this.getData = function(str) {
     if (!data) {
-      var req = new Request();
-      data = req.get(path).then(function(text) {
-        return text;
-      }, function(error) {
-        console.log("Fail to fetch data " + error);
-      });
+      data = setData(this.path).then(parse);
     }
     
-    return data;
+    return data.then(function(data) {
+      var reg = new RegExp(str, "ig");
+      
+      return data.filter(isRegularTrue);
+      
+      function isRegularTrue(value) {
+        return value.City.search(reg) >= 0;
+      };
+    });
+  };
+  
+  function setData(path) {
+    var req = new Request();
+    return req.get(path).then(function(text) {
+      return text;
+    }, function(error) {
+      console.log("Fail to fetch data " + error);
+    });
+  };
+  
+  function parse(text) {
+    return JSON.parse(text);
   };
 }
