4aff5d88ac48556e06e20940939cac06ade85954
Oleg Torokhov <torokhov093@gmail.com>
2017-03-10 09:58:03 +0700

    refactoring, add class Request

---
+++ js/request.js
@@ -0,0 +1,21 @@
+function Request() {
+  this.get = function(path) {
+    return new Promise(function(succeed, fail) {
+      var xhr = new XMLHttpRequest();
+  
+      xhr.open('GET', path, true);
+      xhr.addEventListener("load", function() {
+        if (xhr.status < 400) {
+          succeed(xhr.responseText);
+        } else {
+          fail(new Error("Request failed: " + xhr.statusText));
+        }
+      });
+  
+      xhr.addEventListener("error", function() {
+        fail(new Error("Network error"));
+      });
+      xhr.send(null);
+    });
+  }
+}
